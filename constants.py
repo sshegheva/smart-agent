417e3f3ebcff9362aa77813ac3ee6f2e7ec9c5e8
Snejana Shegheva <sshegheva@dataxu.com>
2015-05-16 20:20:26 -0400

    Improve attributes for the semantic networks solution

--- constants.py
+++ constants.py
@@ -5,7 +5,10 @@ ADD_ACTION = 'add'
 # size actions
 EXPAND_ACTION = 'expand'
 CONTRACT_ACTION = 'contract'
+
 # angle actions
+
+# rotate by 90 degrees, to rotate more - combine rotations
 ROTATE_ACTION = 'rotate'
 REFLECT_ACTION = 'reflect'
 # fill actions
@@ -15,25 +18,31 @@ SLIDE_ACTION = 'slide'
 
 ACTIONS = [UNCHANGED, DELETE_ACTION, ADD_ACTION, EXPAND_ACTION, CONTRACT_ACTION, ROTATE_ACTION, REFLECT_ACTION]
 
-ACTION_WEIGHTS = dict()
+SIMILARITY_WEIGHTS = dict()
 # unchanging is the best transformation
 # so it carries the biggest weight
-ACTION_WEIGHTS[UNCHANGED] = 0.5
+SIMILARITY_WEIGHTS[UNCHANGED] = 5
 # scaling actions are next best
-ACTION_WEIGHTS[EXPAND_ACTION] = 0.4
-ACTION_WEIGHTS[CONTRACT_ACTION] = 0.4
+SIMILARITY_WEIGHTS[REFLECT_ACTION] = 4
+SIMILARITY_WEIGHTS[ROTATE_ACTION] = 3
+SIMILARITY_WEIGHTS[EXPAND_ACTION] = 2
+SIMILARITY_WEIGHTS[CONTRACT_ACTION] = 2
+SIMILARITY_WEIGHTS[FILL_ACTION] = 2
+SIMILARITY_WEIGHTS[DELETE_ACTION] = 1
+SIMILARITY_WEIGHTS[ADD_ACTION] = 1
+SIMILARITY_WEIGHTS[SLIDE_ACTION] = 1
 
 
-INSIDE = 'inside'
-ABOVE = 'above'
-LEFT_OF = 'left_of'
-
-SHAPE = 'shape'
-SIZE = 'size'
-COLOR = 'fill'
-ANGLE = 'angle'
-LOCATION = 'inside'
-ATTRIBUTES = [SHAPE, SIZE, COLOR, ANGLE, LOCATION]
+class Attributes(object):
+    SHAPE = 'shape'
+    SIZE = 'size'
+    COLOR = 'fill'
+    ANGLE = 'angle'
+    INSIDE = 'inside'
+    ALIGNMENT = 'alignment'
+    ABOVE = 'above'
+    LEFT_OF = 'left_of'
+    ATTRIBUTES = [SHAPE, SIZE, COLOR, ANGLE, INSIDE, ALIGNMENT, ABOVE, LEFT_OF]
 
 VERY_SMALL = 'very small'
 SMALL = 'small'
--- solver_utils.py
+++ solver_utils.py
@@ -5,6 +5,8 @@ import operator
 import logging
 import constants
 import problem_utils
+from collections import Counter
+from ProblemSet import ProblemSet
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
@@ -12,31 +14,49 @@ LOGGER.setLevel(logging.DEBUG)
 
 
 def _check_attribute(src, dst, attr):
-    LOGGER.debug('Checking attr {} in {} and {}'.format(attr, src.name, dst.name))
     rules = []
+    # first make sure that attribute exists in both objects
+    if attr not in src.attributes or attr not in dst.attributes:
+        LOGGER.warn('Attribute {} is not present in both objects {} and {}'.format(attr, src.name, dst.name))
+        return rules
     # check that attribute has not changed
     if src.attributes[attr] == dst.attributes[attr]:
         rules.append(constants.UNCHANGED)
     # check the shape
-    elif attr == constants.SHAPE:
+    elif attr == constants.Attributes.SHAPE:
         rules.append(constants.DELETE_ACTION)
         rules.append(constants.ADD_ACTION)
     # check the color
-    elif attr == constants.COLOR:
+    elif attr == constants.Attributes.COLOR:
         rules.append(constants.FILL_ACTION)
     # check the size
-    elif attr == constants.SIZE:
+    elif attr == constants.Attributes.SIZE:
         if constants.SIZES.index(src.attributes[attr]) < constants.SIZES.index(dst.attributes[attr]):
             rules.append(constants.CONTRACT_ACTION)
         else:
             rules.append(constants.EXPAND_ACTION)
     # check the angle
-    elif attr == constants.ANGLE:
-        rules.append(constants.ROTATE_ACTION)
-    elif attr == constants.LOCATION:
+    elif attr == constants.Attributes.ANGLE:
+        degrees_rotated = abs(int(dst.attributes[attr]) - int(src.attributes[attr]))
+        [rules.append(constants.ROTATE_ACTION) for _ in range(degrees_rotated / 90)]
+    elif attr == constants.Attributes.INSIDE:
         # TODO ?
-        rules.append(constants.SLIDE_ACTION)
-    LOGGER.debug('Found rules {}'.format(rules))
+        rules.append(constants.UNCHANGED)
+    # check alignment
+    # can move up, down, right and left and combined
+    elif attr == constants.Attributes.ALIGNMENT:
+        src_move = src.attributes[attr].split('-')
+        dst_move = dst.attributes[attr].split('-')
+        # check position top or bottom
+        if src_move[0] != dst_move[0]:
+            rules.append(constants.SLIDE_ACTION)
+        # check position left or right
+        if src_move[1] != dst_move[1]:
+            rules.append(constants.SLIDE_ACTION)
+    elif attr == constants.Attributes.ABOVE:
+        # TODO ?
+        rules.append(constants.UNCHANGED)
+    LOGGER.debug('Found rules for {} and {} ({}):{}'.format(src.name, dst.name, attr, rules))
     return rules
 
 
@@ -52,8 +72,8 @@ def transformation_rules(source, destination):
     problem_utils.print_raven_object(source.objects)
     problem_utils.print_raven_object(destination.objects)
 
-    # [(a,b)]
-    pairs = zip(source.objects, destination.objects)
+    # [(a,c), (b, d)]
+    pairs = zip(sorted(source.objects.keys()), sorted(destination.objects.keys()))
     rules = []
     for src_name, dst_name in pairs:
         src = source.objects[src_name]
@@ -64,17 +84,26 @@ def transformation_rules(source, destination):
     return rules
 
 
-def similarity_score(ab, cx, alpha=0.5, beta=0.25, gamma=0.25):
+def similarity_score(ab, cx, alpha=0.5, beta=0.25, gamma=0.25, delta=0.25):
     """
     measure the similarity between two rules
 
     similarity = alpha x Size(Sab & Scx)
                     - beta x Size(Sab - Scx)
                     - gamma x Size(Scx - Sab)
+                    - delta x Distance(ab, ax)
     :return: scalar score
     """
-    score = alpha * len(set(ab) & set(cx)) - beta * len(set(ab) - set(cx)) - gamma * len(set(cx) - set(ab))
-    LOGGER.debug('Score for {} and {}: {}'.format(ab, cx, score))
+    LOGGER.debug('Calculating similarity between {} and {}'.format(ab, cx))
+    weighted_similarity_ab = [constants.SIMILARITY_WEIGHTS[rule] * freq for rule, freq in Counter(ab).iteritems()]
+    weighted_similarity_cx = [constants.SIMILARITY_WEIGHTS[rule] * freq for rule, freq in Counter(cx).iteritems()]
+    distance = abs(sum(weighted_similarity_ab) - sum(weighted_similarity_cx))
+    LOGGER.debug('Distance {}'.format(distance))
+    common = set(ab) & set(cx)
+    left = set(ab) - set(cx)
+    right = set(cx) - set(ab)
+    score = alpha * len(common) - beta * len(left) - gamma * len(right) - delta * distance
+    LOGGER.debug('Score: {}'.format(ab, cx, score))
     return score
 
 
@@ -95,3 +124,6 @@ def solve_problem(problem):
     guess = max(scores.iteritems(), key=operator.itemgetter(1))[0]
     LOGGER.debug('Best guess: {}'.format(guess))
     return int(guess)
+
+if __name__ == '__main__':
+    solve_problem(ProblemSet('Basic Problems B').problems[5])
